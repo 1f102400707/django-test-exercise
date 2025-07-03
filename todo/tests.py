e8a3d529e663f4e9f5b8275f713931b07e300446
1F10240070 <s1f102400707@iniad.org>
2025-07-03 11:55:51 +0900

    エラー1

--- todo/tests.py
+++ todo/tests.py
@@ -2,10 +2,14 @@ from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
+
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30,
@@ -17,6 +21,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, 'task1')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
+    
     def test_create_task2(self):
         task = Task(title='task2')
         task.save()
@@ -44,6 +49,8 @@ class TaskModelTestCase(TestCase):
         task = Task(title='task3', due_at=None)
         task.save()
         self.assertFalse(task.is_overdue(current))
+
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -61,6 +68,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
+    
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
@@ -73,6 +81,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
+    
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
@@ -80,7 +89,6 @@ class TodoViewTestCase(TestCase):
         task2.save()
         client = Client()
         response = client.get('/?order=due')
-    
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
